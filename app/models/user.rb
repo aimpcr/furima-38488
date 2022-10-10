5ad2dc7b6fd95987414475bb75b1f0f9f944c4d2
aimpcr <khw_h.mpcr@icloud.com>
2022-10-11 01:36:59 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  validates :password,  format: { with: /\A[a-z0-9]+\z/i }
+
+  validates :family_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
+  validates :kana_family_name,presence: true, format: { with: / \A [ァガー－]+ \z / }
+  validates :kana_first_name,presence: true, format: { with: / \A [ァガー－]+ \z / }
+
 end
