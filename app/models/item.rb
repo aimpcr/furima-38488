bc7677d26094bc58e9c6c6e6153b7b394ccdcd27
aimpcr <khw_h.mpcr@icloud.com>
2022-10-15 17:02:38 +0900

    商品出品機能の実装

---
+++ app/models/item.rb
@@ -0,0 +1,31 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  has_one_attached :image
+
+  belongs_to_active_hash :category 
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_date
+
+
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_date_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+end
