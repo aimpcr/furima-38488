d71753fbf5ed0e075bdcc4b8a2065cee2f2e103f
aimpcr <khw_h.mpcr@icloud.com>
2022-10-22 00:14:40 +0900

    クレジット決済機能の実装

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,18 +1,17 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/}
-
-    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-
+    #トークンのバリデーション
+    validates :token
   end
 
   def save
