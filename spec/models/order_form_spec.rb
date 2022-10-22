a6eb8377d3c9cc4a4a7f3b03bbfe68d3eb1bf254
aimpcr <khw_h.mpcr@icloud.com>
2022-10-22 18:25:07 +0900

    テストの追加

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -81,6 +81,16 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Token can't be blank")
       end
+      it 'user_idが空だと登録できない' do
+        @order_form.user_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと登録できない' do
+        @order_form.item_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Item can't be blank")
+      end
     end
 
   end
